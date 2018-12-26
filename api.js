74359de3d024bd7cfa95d2bf3827b44291f96d11
haozengrun <279768876@qq.com>
2018-12-26 09:54:44 +0800

    no message

--- api.js
+++ api.js
@@ -3,9 +3,11 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 var $sql = require('./sqlMap');//sql语句
+const wx = require('./wxconfig.json'); // 文件中存储了appid 和 secret 微信小程序设置
+const request = require('request'); // 处理node request请求
+var conn = mysql.createConnection(models.mysql); // 连接数据库
+const jwt = require('jsonwebtoken') // 生成签名token
 
-// 连接数据库
-var conn = mysql.createConnection(models.mysql);
 conn.connect(err => {
     if(err){
         console.log(`mysql连接失败: ${err}!`);
@@ -22,8 +24,8 @@ router.use(function (req,res,next) {
     }
     next()
 })
-var jsonWrite = function(obj) {
-    console.log(obj)
+var json = function(obj) {
+    // console.log(obj)
     if(typeof obj.data === 'undefined') {
         obj.res.json({
             code: '1',
@@ -37,17 +39,92 @@ var jsonWrite = function(obj) {
         obj.res.json(responseData);
     }
 };
+let isTokenFn = function(openId) {
+    return new Promise((resolve, reject) => {
+        conn.query(`SELECT * FROM users WHERE openId='${openId}'`, (err, result) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(result)
+            }
+        })
+    })
+}
+// 增加用户登录接口
+router.post('/login', (req, res) => {
+    // var sql = $sql.user.add;
+    var params = req.body;
+    var code = '';
+    if (params.code) {
+        let options = {
+            method: 'POST',
+            url: 'https://api.weixin.qq.com/sns/jscode2session?',
+            formData: {
+                appid: wx.appid,
+                secret: wx.secret,
+                js_code: params.code,
+                grant_type: 'authorization_code'
+            }
+        };
+        request(options, (error, response, body) => {
+            if(error) { // 请求异常时，返回错误信息
+                res.json({
+                    "status": "error",
+                    "code": "ChasenKaso原创文章，转载请注明出处"
+                })
+            } else {
+                let data = JSON.parse(body) // 返回值的字符串转JSON
+                let content = {
+                    openid: data.openid
+                }
+                let token = jwt.sign(content, 'secret', {
+                    expiresIn: 60*60*1  // 1小时过期
+                })
+                if (token) {
+                    isTokenFn(data.openid).then((result) => {
+                        if (result.length < 1) {
+                            conn.query(`INSERT INTO users(openId,token) VALUES ('${content.openid}', '${token}')`,function(err, result) {
+                                if (err) {
+                                    console.log(err);
+                                }
+                                if (result) {
+                                    json({res: res, data: '', msg: '登录成功', code: 1, result: true});
+                                    res.end('is over');
+                                } else {
+                                    json({res: res, data: '', msg: '登录失败', code: 0, result: false});
+                                }
+                            })
+                        } else {
+                            conn.query(`UPDATE users SET token = '${token}' WHERE openId = '${data.openid}' `,function(err, result) {
+                                if (err) {
+                                    console.log(err);
+                                }
+                                if (result) {
+                                    json({res: res, data: '', msg: '更新成功', code: 1, result: true});
+                                    res.end('is over');
+                                }
+                            })
+                        }
+                    }).catch((error) => {
+                        console.log(error)
+                    })
+                }
+            }
+        });
+    } else {
+        json({res: res, data: '', msg: '获取code失败', code: 0, result: false});
+        next()
+    }
+})
 router.post('/getDissSelect', (req, res) => {
     var sql = $sql.disclose.disclose
     var params = req.body
-    console.log(params)
     conn.query(sql, function(err, result) { 
         if (err) {
             console.log(err);
         }
-        console.log(result)
         if (result) {
-            jsonWrite({res: res, data: result, msg: '', code: 1, result: true});
+            json({res: res, data: result, msg: '', code: 1, result: true});
             res.end('is over');
         }
     })
@@ -59,9 +136,8 @@ router.post('/getRankingList', (req, res) => {
         if (err) {
             console.log(err);
         }
-        console.log(result)
         if (result) {
-            jsonWrite({res: res, data: result, msg: '', code: 1, result: true});
+            json({res: res, data: result, msg: '', code: 1, result: true});
             res.end('is over');
         }
     })
@@ -72,7 +148,7 @@ router.post('/addCompany', (req, res) => {
     // var sql2 = $sql.repetition.companyName;
     var params = req.body;
     var sql2 = $sql.name(params.name);
-    conn.query(sql2,function(err, result) {
+    conn.query(sql2, function(err, result) {
         if (err) {
             console.log(err);
         }
@@ -82,49 +158,27 @@ router.post('/addCompany', (req, res) => {
                     console.log(err);
                 }
                 if (result) {
-                    jsonWrite({res: res, data: result, msg: '收到了,老板', code: 1, result: true});
+                    json({res: res, data: result, msg: '收到了,老板', code: 1, result: true});
                     res.end('is over');
                 }
             })
         } else {
-            jsonWrite({res: res, data: '', msg: '我要被你点爆了', code: 1, result: false});
+            json({res: res, data: '', msg: '我要被你点爆了', code: 1, result: false});
         }
     })
 })
-// 增加用户接口
-router.post('/login', (req, res) => {
-    // var sql = $sql.user.add;
-    var params = req.body;
-    console.log(params)
-    // conn.query(sql, function(err, result) {
-    //     if (err) {
-    //         console.log(err);
-    //     }
-    //     if (result) {
-    //         console.log(result)
-    //         // jsonWrite(res, result);
-    //         // for(var i = 0; i < result.length; i++){
-    //         //     console.log("请求回来！",result[i])
-    //         //     console.log("请求结果！",typeof result[i],result[i].userpsw);
-    //         //     if (result[i].userpsw == params.userpsw) {
-    //         //         res.send("返回回来了！");
-    //         //     }
-    //         // }
-    //         res.end('is over');
-    //     }
-    // })
-})
+
 // 点赞接口
 router.post('/addStar', (req, res) => {
     var params = req.body;
     var sql = `UPDATE company SET star = ${params.star} WHERE id = ${params.id}`
-    console.log(params)
+    // console.log(params)
     conn.query(sql, function(err, result) {
         if (err) {
             console.log(err);
         }
         if (result) {
-            jsonWrite({res: res, data: result, msg: '收到了,老板', code: 1, result: true});
+            json({res: res, data: result, msg: '收到了,老板', code: 1, result: true});
             res.end('is over');
         }
     })
--- app.js
+++ app.js
@@ -15,11 +15,31 @@ const httpsOption = {
     key : fs.readFileSync("./1100473_www.haozengrun.com.key"),
     cert: fs.readFileSync("./1100473_www.haozengrun.com.pem")
 }
+const log = require("./config/log");
 // 本地调试的时候开启
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+// logger
+app.all("*", async (req, res, next) => {
+    //响应开始时间
+    const start = new Date();
+    //响应间隔时间
+    var ms;
+    ms = new Date() - start;
+    try {
+      //开始进入到下一个中间件
+      await next();
+      //记录响应日志
+      log.i(req, ms);
+    } catch (error) {
+      //记录异常日志
+      log.e(req, error, ms);
+    }
+    console.log(`${req.method} ${req.url} - ${ms}ms-${res.statusCode}`);
+});
 //服务开启后访问指定编译好的dist文件下的数据
 // app.use(express.static(path.resolve(__dirname, './dist')))
 // app.get('*', function(req, res) {
